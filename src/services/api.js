92c30b2403fa4e137aa3ee6d4b200ab6ab10f29d
themanuca <ssergioca@gmail.com>
2022-03-06 04:20:15 -0300

    ajustes na pi

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,13 @@
 import axios from "axios";
 //require("dotenv/config");
 
-import { getToken } from "./auth";
+//import { getToken } from "./auth";
 
 const api = axios.create({
     url : process.env.REACT_APP_API_URL
 })
 
 
-api.interceptors.request.use(async config => {
-    const token = getToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  });
-  
+
 
 export default api;
